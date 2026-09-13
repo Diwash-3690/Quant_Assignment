a33f93341b38e562795c006e286c6ad953adb784
Diwash Sapkota <ankitdiwash123@gmail.com>
2026-09-13 13:05:38 +0530

    Config: add Next.js, ESLint, and project dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,33 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "coverage/**",
+      "dist/**",
+      "build/**",
+    ],
+  },
+
   js.configs.recommended,
+
   {
     languageOptions: {
       ecmaVersion: 2024,
       sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+  },
+
+  {
+    rules: {
+      "no-unused-vars": "warn",
     },
   },
 ];
